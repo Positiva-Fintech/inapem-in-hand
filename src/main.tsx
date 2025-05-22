06bd971d2fe4c18292d610672d67f644ebe9bb83
Moisés Borracha <moisesnt2@gmail.com>
2025-05-22 13:40:28 +0100

    changed the image path and adjusted the design 6

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { DemoHeroGeometric } from './Demo.tsx'
 // import { SplashCursor } from "@/components/ui/splash-cursor"
+import SplashCursor from './components/ui/SplashCursor.tsx'
 
 
 // export function NoiseDemo() {
@@ -14,7 +15,7 @@ import { DemoHeroGeometric } from './Demo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <SplashCursor /> */}
+    <SplashCursor />
     <DemoHeroGeometric />
   </StrictMode>,
 )
